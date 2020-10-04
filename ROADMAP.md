1c7eaa2b3328a665d9877cb33d4dd1fc6994a1e8
Max Ghenis <mghenis@gmail.com>
2020-10-04 11:48:00 -0700

    First package pieces

---
+++ ROADMAP.md
@@ -0,0 +1,9 @@
+# `scf` roadmap
+
+`scf` currently provides capabilities for loading the Survey of Consumer Finances summary microdata.
+In the future, it will provide more functionality, including:
+* Functions to summarize the microdata, e.g., wealth distributions over time.
+* Charts to visualize these summaries.
+* Standard error calculations for full SCF microdata files.
+
+See the [issues page](https://github.com/MaxGhenis/scf/issues) to view and suggest other items.
---
+++ scf/load.py
@@ -0,0 +1,51 @@
+import microdf as mdf
+import pandas as pd
+
+
+def scf_url(year: int):
+    """ Returns the URL of the SCF summary microdata zip file for a year.
+
+    :param year: Year of SCF summary microdata to retrieve.
+    :type year: int
+    :return: URL of summary microdata zip file for the given year.
+    :rtype: str
+    """
+    return ('https://www.federalreserve.gov/econres/files/scfp' + 
+            str(year) + 's.zip')
+
+
+def load_single_scf(year: int, cols: list):
+    """ Loads SCF summary microdata for a given year and set of columns.
+
+    :param year: Year of SCF summary microdata to retrieve.
+    :type year: int
+    :param cols: List of columns. The weight column `wgt` is always returned.
+    :type cols: list
+    :return: SCF summary microdata for the given year.
+    :rtype: pd.DataFrame
+    """
+    # Add wgt to all returns.
+    cols = list(set(cols) | set(['wgt']))
+    return mdf.read_stata_zip(scf_url(year), columns=cols)
+
+
+def load(years: list, cols: list):
+    """ Loads SCF summary microdata for a set of years and columns.
+
+    :param years: Year(s) to load SCF data for. Can be a list or single number.
+    :type years: list
+    :param cols: List of columns. The weight column `wgt` is always returned.
+    :type cols: list
+    :return: SCF summary microdata for the set of years.
+    :rtype: pd.DataFrame
+    """
+    # If years is a single year rather than a list, return without a loop.
+    if isinstance(years, int):
+        return load_single_scf(year, cols)
+    # Otherwise append to a list within a loop, and return concatenation.
+    scfs = []
+    for year in years:
+        tmp = load_single_scf(year, cols)
+        tmp['year'] = year
+        scfs.append(tmp)
+    return pd.concat(scfs)

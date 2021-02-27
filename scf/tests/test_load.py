3b2d306f1dbc3f9a3f363b3ef89868fe4405bf3b
Max Ghenis <mghenis@gmail.com>
2021-02-27 09:30:57 -0800

    Run black on test

--- scf/tests/test_load.py
+++ scf/tests/test_load.py
@@ -8,18 +8,19 @@ def equal_elements(l1, l2):
 
 def test_load_multiple_years():
     YEARS = [2016, 2019]
-    res = scf.load(YEARS, ['income', 'networth'])
+    res = scf.load(YEARS, ["income", "networth"])
     # Should return the specified columns, plus year and wgt.
-    assert equal_elements(res.columns, ['income', 'networth', 'wgt', 'year'])
+    assert equal_elements(res.columns, ["income", "networth", "wgt", "year"])
     assert equal_elements(res.year.unique().tolist(), YEARS)
 
 
 def test_load_single_year():
     # Test with a single year and single column.
-    res = scf.load(2016, 'networth')
+    res = scf.load(2016, "networth")
     # Should return the specified column, plus wgt (not year).
-    assert equal_elements(res.columns, ['networth', 'wgt'])
+    assert equal_elements(res.columns, ["networth", "wgt"])
+
 
 def test_load_microdf():
-    res = scf.load(2019, 'networth', as_microdataframe=True)
+    res = scf.load(2019, "networth", as_microdataframe=True)
     assert isinstance(res, mdf.MicroDataFrame)
